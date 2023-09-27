6a2bb18bb33e84fd5c9eb9d079ab03208259aeed
Desmond Tay <139625342+qishun6988@users.noreply.github.com>
2023-09-28 01:20:46 +0800

    Simplification of codes

--- Rumus.java
+++ Rumus.java
@@ -2,84 +2,87 @@ import java.util.Scanner;
 
 public class Rumus {
     public static void main(String[] args) {
-
-        // Create a scanner object to read user input
+        // Create a Scanner object for user input
         Scanner input = new Scanner(System.in);
 
-        // Prompt the user to enter the value of 'A' and store it in variable 'a'
-        System.out.println("PLEASE ENTER THE VALUE OF A");
-        int a = input.nextInt();
-      
-        // Prompt the user to enter the value of 'B' and store it in variable 'b'
-        System.out.println("PLEASE ENTER THE VALUE OF B");
-        int b = input.nextInt();
-      
-        // Prompt the user to enter the value of 'C' and store it in variable 'c'
-        System.out.println("PLEASE ENTER THE VALUE OF C");
-        int c = input.nextInt();
+        int a, b, c; // Declare variables to store coefficients A, B, and C of a quadratic equation
 
-        // Check if 'a' is equal to 0
+        // Get valid input for coefficients A, B, and C using a user-defined function
+        a = getUserInput(input, "A");
+        b = getUserInput(input, "B");
+        c = getUserInput(input, "C");
+
+        // Check if 'a' is equal to 0; if so, it's not a valid quadratic equation
         if (a == 0) {
             System.out.println("THE VALUE OF A CANNOT BE 0.");
         } else {
-            // Check the value of 'b'
-            if (b <= -1) {
-                String p = "xx";
-                // Check the value of 'c'
-                if (c <= -1) {
-                    String q = "x";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                } else if (c == 0) {
-                    String q = "";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + "=0");
-                } else {
-                    String q = "x+";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                }
-            } else if (b == 0) {
-                String p = "xx";
-                // Check the value of 'c'
-                if (c <= -1) {
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + c + "=0");
-                } else if (c == 0) {
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + "=0");
-                } else {
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + "+" + c + "=0");
-                }
-            } else if (b >= 1) {
-                String p = "xx+";
-                // Check the value of 'c'
-                if (c <= -1) {
-                    String q = "x";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                } else if (c == 0) {
-                    String q = "x";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + "=0");
-                } else {
-                    String q = "x+";
-                    System.out.println("YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                }
-            }
-            
-            /* Calculation of roots */
-            double d = (b * b) - (4 * a * c);
-            double e = (Math.sqrt(d));
-            double f = -(b) + e;
-            double rumus = f / (2 * a);
-            double g = -(b) - e;
-            double h = g / (2 * a);
+            // Generate and display the quadratic equation
+            String equation = generateQuadraticEquation(a, b, c);
+            System.out.println("YOUR QUADRATIC EQUATION IS: " + equation);
+            // Calculate and display the roots
+            calculateAndDisplayRoots(a, b, c);
+        }
 
-            // Check if the roots are real numbers
-            if (Double.isNaN(rumus) || Double.isNaN(h)) {
-                System.out.println("THE ROOTS ARE NOT REAL.");
-            } else if (rumus == h) {
-                System.out.println("THE ROOT IS " + h + ".");
-            } else {
-                System.out.println("THE ROOTS ARE " + rumus + " AND " + h + ".");
+        // Close the Scanner to free up resources
+        input.close();
+    }
+
+    // Function to get valid user input for a coefficient
+    private static int getUserInput(Scanner input, String variableName) {
+        int value;
+
+        while (true) {
+            System.out.println("PLEASE ENTER THE VALUE OF " + variableName);
+            try {
+                value = input.nextInt(); // Attempt to read an integer from the user
+                System.out.println("THE VALUE OF " + variableName + " IS " + value);
+                break; // Exit the loop if input is valid
+            } catch (Exception e) {
+                // If the input is not a valid integer, catch the exception and provide an error message
+                System.out.println("PLEASE ENTER A VALID NUMBER FOR " + variableName + ".");
+                input.next(); // Clear the invalid input from the input buffer
             }
         }
 
-        // Close the scanner to free up resources
-        input.close();
+        return value; // Return the valid user input
+    }
+
+    // Function to generate the quadratic equation string
+    private static String generateQuadraticEquation(int a, int b, int c) {
+        String p, q;
+
+        // Construct the part of the equation related to 'x^2' and 'x'
+        if (a == 1) {
+            p = (b < 0) ? "x^2" + b + "x" : (b == 0) ? "x^2" : "x^2+" + b + "x";
+        } else if (a == -1) {
+            p = (b < 0) ? "-x^2" + b + "x" : (b == 0) ? "-x^2" : "-x^2+" + b + "x";
+        } else {
+            p = (b < 0) ? a + "x^2" + b + "x" : (b == 0) ? a + "x^2" : a + "x^2+" + b + "x";
+        }
+
+        // Construct the constant part of the equation
+        q = (c < 0) ? c + "=0" : (c == 0) ? "=0" : "+" + c + "=0";
+
+        // Combine the parts to create the complete quadratic equation
+        return p + q;
+    }
+
+    private static void calculateAndDisplayRoots(int a, int b, int c) {
+        /* Calculation of roots */
+        double d = (b * b) - (4 * a * c); // Calculate the discriminant
+        double e = (Math.sqrt(d)); // Calculate the square root of the discriminant
+        double f = -(b) + e; // Calculate the first root
+        double rumus = f / (2 * a); // Calculate the value of the first root
+        double g = -(b) - e; // Calculate the second root
+        double h = g / (2 * a); // Calculate the value of the second root
+
+        // Check if the roots are real numbers
+        if (Double.isNaN(rumus) || Double.isNaN(h)) {
+            System.out.println("THE ROOTS ARE NOT REAL.");
+        } else if (rumus == h) {
+            System.out.println("THE ROOT IS " + h + ".");
+        } else {
+            System.out.println("THE ROOTS ARE " + rumus + " AND " + h + ".");
+        }
     }
 }
--- Rumus_GUI.java
+++ Rumus_GUI.java
@@ -2,71 +2,69 @@ import javax.swing.JOptionPane;
 
 public class Rumus_GUI {
     public static void main(String[] args) {
-
         // Input coefficients A, B, and C using pop-up dialog boxes
         int a = Integer.parseInt(JOptionPane.showInputDialog("PLEASE ENTER THE VALUE OF A"));
         int b = Integer.parseInt(JOptionPane.showInputDialog("PLEASE ENTER THE VALUE OF B"));
         int c = Integer.parseInt(JOptionPane.showInputDialog("PLEASE ENTER THE VALUE OF C"));
 
-        // Check if the coefficient 'a' is equal to 0
+        // Process the quadratic equation with the provided coefficients
+        processQuadraticEquation(a, b, c);
+    }
+
+    private static void processQuadraticEquation(int a, int b, int c) {
         if (a == 0) {
+            // If A is 0, it's not a valid quadratic equation
             JOptionPane.showMessageDialog(null, "THE VALUE OF A CANNOT BE 0.");
         } else {
-            // Check the value of coefficient 'b'
-            if (b <= -1) {
-                String p = "xx";
-                // Check the value of coefficient 'c'
-                if (c <= -1) {
-                    String q = "x";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                } else if (c == 0) {
-                    String q = "";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + "=0");
-                } else {
-                    String q = "x+";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                }
-            } else if (b == 0) {
-                String p = "xx";
-                // Check the value of coefficient 'c'
-                if (c <= -1) {
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + c + "=0");
-                } else if (c == 0) {
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + "=0");
-                } else {
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + "+" + c + "=0");
-                }
-            } else if (b >= 1) {
-                String p = "xx+";
-                // Check the value of coefficient 'c'
-                if (c <= -1) {
-                    String q = "x";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                } else if (c == 0) {
-                    String q = "x";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + "=0");
-                } else {
-                    String q = "x+";
-                    JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS : " + a + p + b + q + c + "=0");
-                }
-            }
+            // Generate the quadratic equation string
+            String equation = generateQuadraticEquation(a, b, c);
+            
+            // Display the quadratic equation to the user
+            JOptionPane.showMessageDialog(null, "YOUR QUADRATIC EQUATION IS: " + equation);
 
-            /* Calculation of roots */
-            double d = (b * b) - (4 * a * c);
-            double e = (Math.sqrt(d));
-            double f = -(b) + e;
-            double rumus = f / (2 * a);
-            double g = -(b) - e;
-            double h = g / (2 * a);
+            // Calculate and display the roots of the equation
+            calculateAndDisplayRoots(a, b, c);
+        }
+    }
 
-            // Check if the roots are real numbers
-            if (Double.isNaN(rumus) || Double.isNaN(h)) {
-                JOptionPane.showMessageDialog(null, "THE ROOTS ARE NOT REAL.");
-            } else if (rumus == h) {
-                JOptionPane.showMessageDialog(null, "THE ROOT IS " + h + ".");
-            } else {
-                JOptionPane.showMessageDialog(null, "THE ROOTS ARE " + rumus + " AND " + h + ".");
-            }
+    private static String generateQuadraticEquation(int a, int b, int c) {
+        String p, q;
+
+        if (a == 1) {
+            // Handle the case where A is 1
+            p = (b < 0) ? "x^2" + b + "x" : (b == 0) ? "x^2" : "x^2+" + b + "x";
+        } else if (a == -1) {
+            // Handle the case where A is -1
+            p = (b < 0) ? "-x^2" + b + "x" : (b == 0) ? "-x^2" : "-x^2+" + b + "x";
+        } else {
+            // Handle other values of A
+            p = (b < 0) ? a + "x^2" + b + "x" : (b == 0) ? a + "x^2" : a + "x^2+" + b + "x";
+        }
+
+        // Construct the constant part of the equation
+        q = (c < 0) ? c + "=0" : (c == 0) ? "=0" : "+" + c + "=0";
+
+        // Combine the parts to create the complete quadratic equation
+        return p + q;
+    }
+
+    private static void calculateAndDisplayRoots(int a, int b, int c) {
+        double d = (b * b) - (4 * a * c); // Calculate the discriminant
+        double e = Math.sqrt(d); // Calculate the square root of the discriminant
+        double f = -(b) + e; // Calculate the first root
+        double rumus = f / (2 * a); // Calculate the value of the first root
+        double g = -(b) - e; // Calculate the second root
+        double h = g / (2 * a); // Calculate the value of the second root
+
+        // Check if the roots are real numbers
+        if (Double.isNaN(rumus) || Double.isNaN(h)) {
+            JOptionPane.showMessageDialog(null, "THE ROOTS ARE NOT REAL.");
+        } else if (rumus == h) {
+            // If both roots are the same
+            JOptionPane.showMessageDialog(null, "THE ROOT IS " + h + ".");
+        } else {
+            // If there are two distinct roots
+            JOptionPane.showMessageDialog(null, "THE ROOTS ARE " + rumus + " AND " + h + ".");
         }
     }
 }
