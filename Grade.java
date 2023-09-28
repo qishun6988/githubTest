9d11369128668e3b209945a9da9e82dccbde2406
Desmond Tay <139625342+qishun6988@users.noreply.github.com>
2023-09-29 03:38:40 +0800

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class Grade {
-    public static void main(String args[]) {
-      
+
+    public static void main(String[] args) {
+
         // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
-      
-        // Prompt the user to enter their grade
-        System.out.println("PLEASE ENTER YOUR GRADE");
-        
-        // Read the first character of the input and store it in the 'grade' variable
-        char grade = scanner.next().charAt(0);
- 
+
+        // Initialize grade as a single space character
+        char grade = ' ';
+
+        // Keep prompting the user for a grade until a valid one is entered
+        while (grade == ' ' || grade == '\t' || grade == '\n') {
+            System.out.print("PLEASE ENTER YOUR GRADE: "); // Prompt the user for input
+            String input = scanner.nextLine(); // Read the user's input as a string
+
+            // Check if the input length is 1 or 2 characters
+            if (input.length() == 1 || input.length() == 2) {
+                grade = input.charAt(0); // Assign the first character to 'grade'
+            } else {
+                System.out.println("PLEASE ENTER VALID GRADES IN MALAYSIA’S GRADING SYSTEM");
+                // Display an error message if the input is not 1 or 2 characters long
+            }
+        }
+
         // Use a switch statement to determine and display a message based on the grade
         switch (grade) {
             case 'A':
@@ -43,9 +55,10 @@ public class Grade {
                 System.out.println("YOU NEED TO STUDY HARDER\nYOUR GRADE IS G");
                 break;
             default:
-                System.out.println("UNKNOWN GRADE");
+                System.out.println("PLEASE ENTER VALID GRADES IN MALAYSIA’S GRADING SYSTEM");
+                // Display an error message for an invalid grade
         }
-        
+
         // Close the Scanner to free up resources
         scanner.close();
     }
